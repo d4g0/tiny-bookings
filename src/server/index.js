aa518d1332f0d5722917486c98e72d15e08d0ecc
d4g0 <tocarralero@gmail.com>
2022-03-27 11:25:25 -0400

    basic graphql setup

--- src/server/index.js
+++ src/server/index.js
@@ -4,30 +4,36 @@ const app = Express();
 import router from '~/server/router';
 import { apiRateLimiter } from '~/server/middleware/index.js';
 import helmet from 'helmet';
-
+import graphqlServer from '~/server/graphql'
+import expressPlayground from 'graphql-playground-middleware-express';
 
 
 // app settings 
-app.set('trust proxy', 1); // behind proxy setting
+// behind proxy setting
+app.set('trust proxy', 1); 
 // general middlewares
-app.use(apiRateLimiter, helmet());
+if (process.env.NODE_ENV == 'production') {
+    app.use(apiRateLimiter, helmet());
+}
 app.use(Express.json());
 // api routes
 app.use('/api/', router);
 
+// graphql
+async function setupGraphQl() {
+    await graphqlServer.start();
+    graphqlServer.applyMiddleware({ app });
+}
 
 // app logs
 // TODO define
 
-
-
-
-
-
-
-
-
-
 export function spinUpServer() {
+    setupGraphQl();
+
+    // set up graphql playground
+    if (process.env.NODE_ENV != 'production') {
+        app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+    }
     app.listen(API_PORT, () => { console.log('app listening at port:' + API_PORT) })
 }
