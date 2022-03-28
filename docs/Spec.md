e427a9e3b852e7e567b429768be3f4715f38c18f
d4g0 <tocarralero@gmail.com>
2022-03-28 04:05:12 -0400

    [db-schema-iteration] admin.email (unique and required)

--- docs/Spec.md
+++ docs/Spec.md
@@ -1,9 +1,19 @@
-## Internal Data models (Services and up layer, DAO handle db structure internals)
+## Internal Data models 
+**Services and up layer, DAO handle db structure internals**
+
+### USER_ROLES
+-   FULL_ADMIN
+-   BASIC_ADMIN
+-   CLIENT
+
+
 ### ADMIN
 ```js
-{
+{   
+    id:                 number, // integer
     user_role:          USER_ROLES.FULL_ADMIN || USER_ROLES.BASIC_ADMIN,
     admin_name:         String,
+    email:              String,
     admin_description:  String,
     hash_password:      String,
     reset_token:        String || null,
--- docs/sql/init-db-values.sql
+++ docs/sql/init-db-values.sql
@@ -1,19 +1,21 @@
 -- user roles
-insert into user_roles ( user_role ) values ( 'full-admin'), ('basic-admin'), ('client');
+insert into user_roles ( user_role ) values ( 'FULL_ADMIN'), ('BASIC_ADMIN'), ('CLIENT');
 -- db like
--- full-admin  1
--- basic-admin 2
--- client      3
+-- FULL_ADMIN  1
+-- BASIC_ADMIN 2
+-- CLIENT      3
 
 -- default admin (TODO: change where deploying ok)
 insert into admins (
     user_role,
     admin_name,
+    email,
     admin_description,
     hash_password
  ) values (
      1,
      'dago',
+     'tocarralero@gmail.com',
      'system creator',
      '$2a$10$2nZp/EGj.aQY/PyyqGAMze2.a1C4H1knpdNSAO.TX/92katjMo75C'
  );
