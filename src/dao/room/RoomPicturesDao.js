bdeffeafe5a90945a6d5847cca86f405285fe407
d4g0 <tocarralero@gmail.com>
2022-04-06 23:27:34 -0400

    [refactor] (separation of concerns) (dao)(roomPictures) Create and delete

---
+++ src/dao/room/RoomPicturesDao.js
@@ -0,0 +1,57 @@
+import { prisma } from "~/dao/PrismaClient";
+import { isValidId } from "dao/utils";
+import { isValidString } from "utils";
+
+
+export async function createARoomPicture(room_id, filename) {
+
+    if (!isValidId(room_id)) {
+        throw new Error('Non Valid Id');
+    }
+
+    if (!isValidString(filename)) {
+        throw new Error('Non Filename');
+    }
+
+
+
+    try {
+        // create the room picture
+        var roomPicture = await prisma.room_pictures.create({
+            data: {
+                room_id,
+                filename
+            }
+        })
+
+        return roomPicture;
+
+    } catch (error) {
+        throw error
+    }
+}
+
+export async function deleteARoomPicture(room_picture_id) {
+
+    if (!isValidId(room_picture_id)) {
+        throw new Error('Non Valid Id');
+    }
+
+
+
+
+
+    try {
+        // create the room picture
+        var roomPicture = await prisma.room_pictures.delete({
+            where: {
+                id: room_picture_id
+            }
+        })
+
+        return roomPicture;
+
+    } catch (error) {
+        throw error
+    }
+}
