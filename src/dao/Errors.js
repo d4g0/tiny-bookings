b7b18055319037ab5a7ec0d7493f1e874971fb6d
d4g0 <tocarralero@gmail.com>
2022-04-07 15:57:01 -0400

    better error handling, targets, to signal consumer what is failing

--- src/dao/Errors.js
+++ src/dao/Errors.js
@@ -1,14 +1,21 @@
 
 /**
- * Unique constraint
+ * Unique constraint.
+ * Prisma Sample Exeption:
+ * ```js
+ * code: 'P2002',
+ * clientVersion: '3.11.0',
+ * meta: { target: [ 'room_name' ] }
+ * ```
  */
 // key
 export const DB_UNIQUE_CONSTRAINT_ERROR_KEY = 'DB_UNIQUE_CONSTRAINT_ERROR';
 // class
 export class DB_UNIQUE_CONSTRAINT_ERROR extends Error {
-    constructor(message) {
-        super(message);
+    constructor(message, target = '') {
+        super(message, target);
         this.code = DB_UNIQUE_CONSTRAINT_ERROR_KEY;
+        this.target = target;
     }
 }
 
