25d7035f52ee6ef6b066cfd692c0d79bda2d0823
d4g0 <tocarralero@gmail.com>
2022-04-09 16:50:14 -0400

    InitUserRoles inclusion in booting secuence secuence

--- test/config/setup.js
+++ test/config/setup.js
@@ -1,8 +1,11 @@
-const connect = require('../../src/dao/PrismaClient.js')
+const { connect } = require('../../src/dao/PrismaClient')
+const { initUserRoles } = require('../../src/dao/users/UserRoleDao.js')
 module.exports = async function setup() {
     try {
         await connect();
+        await initUserRoles();
     } catch (error) {
-
+        console.log(error);
+        throw error;
     }
 }
