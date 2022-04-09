8f37de541e8c1159b08375c3e629f92429400864
d4g0 <tocarralero@gmail.com>
2022-04-09 00:28:56 -0400

    [ anotation ] Deprecate use of id

--- src/dao/DBConstans.js
+++ src/dao/DBConstans.js
@@ -1,6 +1,8 @@
 // This constans shoul be fullfiled in the data-base, 
 // in the precise same order as in the `init-db-values.sql` file
-// 
+// TODO
+// Dprecate the use of the `id` field
+// will be dinamyc as is db state dependent
 export const USER_ROLES = {
     'FULL_ADMIN': {
         id: 1,
