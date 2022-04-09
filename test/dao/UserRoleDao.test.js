94cfc5c8ee7ba9d1495b255d2aa83c377ab58a6e
d4g0 <tocarralero@gmail.com>
2022-04-09 00:03:25 -0400

    [test enhance]

--- test/dao/UserRoleDao.test.js
+++ test/dao/UserRoleDao.test.js
@@ -44,5 +44,26 @@ describe(
 
             }
         )
+
+        test(
+            "Test Get Res when querying for a non existing user role",
+            async function () {
+                var dbError = null, userRole = null, RANDOM_KEY = uuid().substring(0, 10);
+
+                try {
+                    userRole = await getUserRoleByKey(RANDOM_KEY);
+                    console.log({
+                        userRole
+                    });
+                } catch (error) {
+                    console.log(error);
+                    dbError = error;
+                }
+                // normal behavior
+                expect(dbError).toBeNull()
+                // assertions
+                expect(userRole).toBeNull();
+            }
+        )
     }
 )
