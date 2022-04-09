2fe4f58e58bf47aa91df92e97f0d12c648188899
d4g0 <tocarralero@gmail.com>
2022-04-09 02:17:18 -0400

    initUserRoles for booting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { connect, disconnect } from 'dao/PrismaClient.js'
+import { initUserRoles } from 'dao/users/UserRoleDao';
 import { spinUpServer } from '~/server'
 async function main() {
 
     // connect to db
     await connect();
+    // init db
+    await initUserRoles();
     // spin up the server
     spinUpServer();
 }
