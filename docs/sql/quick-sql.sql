442a07107a1904af861acabb7bbb32e1897c626c
d4g0 <tocarralero@gmail.com>
2022-04-09 22:23:39 -0400

    composed primary keys exercise

--- docs/sql/quick-sql.sql
+++ docs/sql/quick-sql.sql
@@ -4,3 +4,30 @@ insert into admins ( user_role, name, description, hash_password)
 values ( 1,'dago', 'system creator', 'supper-foo-hash');
 end;
 select * from admins;
+
+
+begin;
+create table room (
+	id serial primary key,
+	room_name varchar(30)
+);
+
+create table amenity (
+	id serial primary key,
+	amenity varchar(30)
+);
+
+create table rooms_amenities (
+	room_id 	int REFERENCES room (id),
+	amenity_id  int REFERENCES amenity (id),
+	PRIMARY KEY (room_id, amenity_id)
+);
+
+
+end;
+
+insert into room (room_name) values ('Blue Room');
+insert into amenity (amenity) values ('Hot Top');
+insert into rooms_amenities ( room_id, amenity_id ) values ( 1 , 1);
+
+select * from rooms_amenities;
