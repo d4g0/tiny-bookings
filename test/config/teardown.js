1f8f9b52a483be8b7bcc49dfe73244a6b7df1eb2
d4g0 <tocarralero@gmail.com>
2022-04-15 20:44:37 -0400

    investigate this, end does not do anithing

--- test/config/teardown.js
+++ test/config/teardown.js
@@ -1,9 +1,11 @@
 const { disconnect } = require('../../src/dao/PrismaClient.js');
-const sql = require('../../src/dao/postgres');
+const sql = require('../../src/db/postgres.js');
+const { disconnectPostgres } = require('../../src/db/postgres.js');
 module.exports = async function teardown() {
     try {
         await disconnect();
-        await sql.default.end({ timeout: 1 });
+        // console.log(sql);
+        await disconnectPostgres();
         console.log('tear down');
     } catch (error) {
         console.log(error)
