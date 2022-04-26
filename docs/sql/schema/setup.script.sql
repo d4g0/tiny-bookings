2c4446fac2f02eee9bea11df2507c578aac0e3c0
d4g0 <tocarralero@gmail.com>
2022-04-26 04:56:07 -0400

    fn incorporation getRoomData

--- docs/sql/schema/setup.script.sql
+++ docs/sql/schema/setup.script.sql
@@ -376,4 +376,58 @@ end loop;
 END;
 $$
 LANGUAGE plpgsql;
+
+
+create or replace function get_room_data(
+	room_id_filter integer
+) returns room_data as 
+$$
+
+DECLARE
+temp_room_data room_data%rowtype;
+
+BEGIN
+
+select * into temp_room_data from (
+	select
+	rm.id,
+	rm.hotel_id,
+	rm.room_name,
+	rm.night_price,
+	rm.capacity,
+	rm.number_of_beds,
+	rm.created_at,
+	rm.room_type as room_type_id,
+-- room type
+	( 
+		select rt.room_type from room_types rt
+		where rt.id = rm.room_type
+	) as room_type_key,
+-- 	room pictures
+	ARRAY(
+		select 
+			rp.id || ' ' || rp.filename
+		from room_pictures rp
+		where rp.room_id = rm.id
+	) as room_pictures,
+-- 	room amenities
+	ARRAY(
+		select 
+		 ra.id || ' ' || ra.amenity
+		from room_amenity ra join rooms_amenities rams
+		on ra.id = rams.amenity_id
+		where rams.room_id = rm.id
+	) as room_amenities
+	from room rm 
+	where rm.id = room_id_filter
+) room_data;
+
+return temp_room_data;
+
+END;
+$$
+LANGUAGE plpgsql;
+
+
+
 END;
